5b9a90ba62dfb8266d7927e1b3f49b4811b17f9e
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-01-24 13:40:04 +1100

    Added turn button

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,15 @@ document.documentElement.style.setProperty("--square-size", SQUARESIZE.toString(
 
 const grid = <HTMLElement>document.querySelector(".window");
 const cells = generateMap(GRIDSIZE, grid);
+
+const popup = <HTMLElement>document.querySelector(".resource-type-popup");
+const playTurn = document.querySelector("button.play-turn");
+playTurn?.addEventListener("mouseover", () => {
+	popup.style.display = "flex";
+	popup.textContent = "Finish Turn";
+})
+
+playTurn?.addEventListener("mouseout", () => {
+	popup.style.display = "none";
+	popup.textContent = "";
+})
